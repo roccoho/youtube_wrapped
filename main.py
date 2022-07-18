0017ea0f41532d3958e40494f8d279cbfa157194
rocco <103323204+roccoho@users.noreply.github.com>
2022-07-19 02:35:05 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -41,23 +41,21 @@ def intro():
     return render_template('intro.html')
 
 
-@app.route('/projectid_date', methods=['POST'])
-def projectid():
+@app.route('/api_key_date', methods=['POST'])
+def api_key():
     if request.method == 'POST':
-        projectid = request.form['projectid']
+        api_key = request.form['api_key']
         datefrom = request.form['datefrom']
         dateto = request.form['dateto']
         dates = [datefrom, dateto]
         print(dates)
-        test_id = youtube_api.video_to_json('dQw4w9WgXcQ', projectid)
+        test_id = youtube_api.video_to_json('dQw4w9WgXcQ', api_key)
         if test_id:  # test if project id works
             dates = data_col.filter_watch_hist_date(datefrom, dateto)
             if dates:
-                start_time = t.perf_counter()
-                response = data_col.get_info(projectid)
+                response = data_col.get_info(api_key)
                 data_col.video_json_to_csv(response)
-                print(f'total exec: {t.perf_counter() - start_time}')
-                panda_man.manipulate_data(projectid)
+                panda_man.manipulate_data(api_key)
                 data = csv_to_json(
                     csv_name=['stats', 'video_hist', 'channel_hist', 'tags_hist', 'hour_hist',
                               'month_hist', 'day_hist', 'week_hist'], folder='stats')
@@ -65,9 +63,9 @@ def projectid():
             else:
                 error = "Date error"
         else:
-            error = "Project ID invalid"
+            error = "Invalid API key"
 
-    return render_template('project_id.html', dates=dates, error=error)
+    return render_template('api_key.html', dates=dates, error=error)
 
 
 @app.route('/display', methods=['POST'])
@@ -81,7 +79,7 @@ def save_file():
             dates = data_col.watch_hist_json_to_csv(f'{UPLOAD_FOLDER}{filename}')
 
             if dates:
-                return render_template('project_id.html', dates=dates)
+                return render_template('api_key.html', dates=dates)
             else:
                 error = 'Wrong JSON file'
 
