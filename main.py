0f1968c8a5a6cde058864f14dcdb2b98fdbb5165
rocco <103323204+roccoho@users.noreply.github.com>
2022-07-28 01:51:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from werkzeug.utils import secure_filename
 import data_col
 import youtube_api
 import panda_man
-import time as t
 
 # fix empty x axis
 # create executable
@@ -37,9 +36,9 @@ def csv_to_json(csv_name, folder=''):
     return dicts
 
 
-# @app.route('/')
-# def intro():
-#     return render_template('intro.html')
+@app.route('/')
+def intro():
+    return render_template('intro.html')
 
 
 @app.route('/api_key_date', methods=['POST'])
@@ -90,17 +89,17 @@ def save_file():
     return render_template('intro.html', error=error)
 
 
-@app.route('/')
-def intro():
-    data = csv_to_json(
-        csv_name=['stats', 'video_hist', 'channel_hist', 'tags_hist', 'hour_hist',
-                  'month_hist', 'day_hist', 'week_hist', 'week_hour_hist'], folder='stats')
-    return render_template('result.html', data=data)
+# @app.route('/')
+# def intro():
+#     data = csv_to_json(
+#         csv_name=['stats', 'video_hist', 'channel_hist', 'tags_hist', 'hour_hist',
+#                   'month_hist', 'day_hist', 'week_hist', 'week_hour_hist'], folder='stats')
+#     return render_template('.html', data=data)
 
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.run(host="127.0.0.1", port=6969, debug=True)
+    app.run(debug=False)
 
